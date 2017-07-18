5a2d9ed75b88f41c38a704bf75ccd9f5a1bf3216
zhuang-hao-ming <csuhaoming@163.com>
2017-07-18 16:33:14 +0800

    rewrite the make_track through pl/pgsql to improve the performance

--- make_track.sql
+++ make_track.sql
@@ -1,6 +1,6 @@
 ﻿
 
-
+-- idicate whether two consective gps_logs are belong to the same track
 CREATE OR REPLACE FUNCTION test_time_dis_constrain(pre_x FLOAT, pre_y FLOAT, now_x FLOAT, now_y FLOAT, pre_time TIMESTAMP, now_time TIMESTAMP, time_threshold INTEGER DEFAULT 120, speed_threshold INTEGER DEFAULT 33)
 RETURNS BOOLEAN 
 AS
@@ -9,17 +9,25 @@ DECLARE
 	delta INTEGER := 0;
 	dis FLOAT := 0;
 BEGIN
+	-- the interval of two logs
 	SELECT EXTRACT (EPOCH FROM (pre_time - now_time)) INTO delta;
+	delta := abs(delta);
+
+	-- debug message
+	-- RAISE INFO 'pre_time %, now_time %, pre_x % , pre_y %, now_x %, now_y % ,delta %', pre_time, now_time, pre_x, pre_y, now_x, now_y, delta;
+
+	
 	IF delta > time_threshold THEN
 		return false;
 	END IF;
+	
 	dis = (pre_x - now_x) ^ 2 + (pre_y - now_y) ^ 2;
+	
 	IF dis > (delta * speed_threshold) ^ 2 THEN
 		return false;
 	END IF;
 	return true;	
 END;
-
 
 $$
 LANGUAGE plpgsql;
@@ -27,22 +35,16 @@ LANGUAGE plpgsql;
 
 
 
---- make track
-
+--- make track table
+--- this function iterate the gps_log_valid table in order and make track according to criterias given by test_time_dis_constrain function
 CREATE OR REPLACE FUNCTION make_track()
-RETURNS TABLE 
-(
-   id BIGINT,
-   gps_log_ids INT[]
-) 
+RETURNS VOID 
 AS $$
 DECLARE 
-	titles TEXT DEFAULT '';
-	uuid BIGINT DEFAULT 1;
 	log_ids INT[];
 	pre_log RECORD;
 	cur_log RECORD;
-	cur_logs CURSOR FOR SELECT id,log_time, car_id, direction AS v, ST_X(geom) AS x, ST_Y(geom) AS y  FROM gps_log_valid ORDER BY car_id, log_time LIMIT 10;
+	cur_logs CURSOR FOR SELECT id,log_time, car_id, direction AS v, ST_X(geom) AS x, ST_Y(geom) AS y  FROM gps_log_valid ORDER BY car_id, log_time;
 BEGIN
 	-- Open the gps log cursor
 	OPEN cur_logs;
@@ -50,35 +52,57 @@ BEGIN
 	FETCH cur_logs INTO pre_log;
 	log_ids := ARRAY[pre_log.id];
 	LOOP
-		uuid := uuid + 1;
 		-- fetch row into the rec_log
 		FETCH cur_logs INTO cur_log;
-		
-		IF NOT cur_log THEN
-			IF array_length(log_ids, 1) > 0 THEN
-				RETURN NEXT (uuid, log_ids)::RECORD;
-			EXIT;
-		END IF;
-		
-		IF test_time_dis_constrain(pre_log.x, pre_log.y, cur_log.x, cur_log.y, pre_log.log_time, now_log.log_time) THEN
-			array_append(log_ids, cur_log.id);
+		EXIT WHEN NOT FOUND;
+					
+		IF test_time_dis_constrain(pre_log.x, pre_log.y, cur_log.x, cur_log.y, pre_log.log_time, cur_log.log_time) THEN
+			log_ids := array_append(log_ids, cur_log.id);
 			pre_log := cur_log;
-		ELSE THEN
-			RETURN NEXT (uuid, log_ids)::RECORD;
+			--RAISE INFO 'append %', cur_log.id;
+			
+		ELSE	
+			--RAISE INFO 'insert %', log_ids;
+			INSERT INTO tracks_1(points) VALUES(log_ids);
 			log_ids := ARRAY[cur_log.id];
 			pre_log := cur_log;
 		END IF;
-
-		
-		-- exit when no more row to fetch
-		
-
-		
+				
 	END LOOP;
+
+	IF array_length(log_ids, 1) > 0 THEN	
+		INSERT INTO tracks_1(points) VALUES(log_ids);
+		--RAISE INFO 'end insert %', log_ids;
+	END IF;
   
    -- Close the cursor
-   CLOSE cur_films;
- 
-   RETURN titles;
+	CLOSE cur_logs;
+
+	RETURN;
 END; $$
 LANGUAGE plpgsql;
+
+
+-- create the track table
+CREATE TABLE tracks_1
+(
+id SERIAL PRIMARY KEY,
+points INTEGER[]
+)
+-- invode the function
+SELECT make_track();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
